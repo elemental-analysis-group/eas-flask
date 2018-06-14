0bedfe6cb14a4c53592ab0c9509870e19d3ff4fe
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-06-14 15:34:26 -0300

    new class

--- app/controllers/CalibrationController.py
+++ app/controllers/CalibrationController.py
@@ -12,6 +12,7 @@ from app.models.CalibrationFiles import CalibrationFiles
 from app.models.User import User
 
 from app.forms.CalibrationForm import CalibrationForm, CalibrationFormFiles
+from app.utils.RF import RF
 
 import Micromatter
 import WinQxas
@@ -77,42 +78,8 @@ def showCalibration(id):
     # get all uploaded files from this calibration
     uploads = CalibrationFiles.query.filter_by(calibration_id=calibration.id).all()
     
-    ######################################## TODO: migrate this code to an external function
-    # adding micrommater info
-    info = {}
-    ResponseFactors = {}
-    elements= {}
-    for i in uploads:
+    info, ResponseFactors, elements = RF(uploads)
 
-        info[i.micromatter_id] = Micromatter.get(i.micromatter_id)
-
-        # txt
-        txt_content = pathlib.Path( app.config['FILES'] + '/' + i.txt_file).read_text()
-        txt_info = WinQxas.parseTxt(txt_content)
-
-        # csv
-        csv_content = pathlib.Path( app.config['FILES'] + '/' + i.csv_file).read_text()
-        csv_info = Shimadzu.parseCsv(csv_content)
-
-        elements[i.micromatter_id] = {}
-        ResponseFactors[i.micromatter_id] = {}
-
-        elements[i.micromatter_id] = [ x for x in info[i.micromatter_id].keys() if x is not 'total']
-
-        for element in elements[i.micromatter_id]:
-            # se tiver espectro para o elemento em questão, calcula, senão passa direto
-            try:
-                Z = element
-                density = float(info[i.micromatter_id][element])    
-                N = txt_info['K']['peaks'][Z]
-                ResponseFactors[i.micromatter_id][Z] = ResponseFactor(float(N),density,csv_info['current'],csv_info['livetime'])
-
-            except:
-               pass
-
-
-    #####################################################################
-    
     return render_template('calibration/show.html',
             calibration=calibration,
             form=form,
